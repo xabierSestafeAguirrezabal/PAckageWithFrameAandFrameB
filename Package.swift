43a3b07733304ad0158f0aec72feea73f99c064f
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-09-02 11:37:14 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "PAckageWithFrameAandFrameB",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PAckageWithFrameAandFrameB",
             targets: ["FrameworkAPrivate", "FrameworkBPrivate"]),
